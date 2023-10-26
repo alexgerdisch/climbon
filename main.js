586ea3d18ee329eee0934e161621f442a1674cbf
alexgerdisch <alexgerdisch@gmail.com>
2023-10-25 17:02:27 -0700

    neurel net imported

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from '@tensorflow/tfjs-core';
+// Register one of the TF.js backends.
+import '@tensorflow/tfjs-backend-webgl';
+import '@mediapipe/pose';
+import '@tensorflow/tfjs-backend-webgpu';
+
 document.getElementById("film").addEventListener("change", function() {
   let media = URL.createObjectURL(this.files[0]);
   let video = document.getElementById("filmDisplay");
